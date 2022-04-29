690cf80f9e3f11f24b053dcd104308c57e684bb1
Alon <57628667+AlonHor@users.noreply.github.com>
2022-04-29 17:23:24 +0300

    Added counters and improved clearCompletedTasks().

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,117 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const clear = document.querySelector<HTMLButtonElement>('#clear');
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+  saveTasks();
+
+  addListItem(newTask);
+  input.value = '';
+});
+
+clear?.addEventListener('click', clearCompletedTasks);
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.classList.add('checkbox');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    if (task.completed) {
+      label.classList.add('completed');
+    } else {
+      label.classList.remove('completed');
+    }
+    saveTasks();
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  if (checkbox.checked) {
+    label.classList.add('completed');
+  }
+  const title = document.createElement('span');
+  title.textContent = task.title;
+  title.classList.add('text');
+  label.append(checkbox, title);
+  item.append(label);
+  list?.append(item);
+}
+
+function clearCompletedTasks() {
+  console.groupCollapsed(
+    '%cCLEARING...',
+    'color:#ffff00;font-family:system-ui;font-size:1.5rem;font-weight:bold'
+  );
+  console.trace('clear');
+  console.time('clear');
+
+  const completedTasks = tasks.filter((t) => t.completed);
+  completedTasks.forEach((t) => {
+    console.count('For each completed task');
+    // const index = tasks.findIndex((t2) => t2.id === t.id);
+    const index = tasks.indexOf(t);
+    tasks.splice(index, 1);
+  });
+
+  console.assert(
+    tasks.filter((t) => t.completed).length === 0,
+    '%cERROR %cFAILED TO REMOVE COMPLETED TASKS',
+    'color:#ff0000;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold',
+    'font-family:system-ui;font-size:1.5rem;font-weight:bold'
+  );
+  console.timeEnd('clear');
+  console.groupEnd();
+  saveTasks();
+  list!.innerHTML = '';
+  tasks.forEach(addListItem);
+
+  if (tasks.filter((t) => t.completed).length > 0) {
+    console.error(
+      '%cFAILED! %cSEE %cCLEARING %cGROUP FOR DETAILS.',
+      'color:#ff0000;font-family:system-ui;font-size:4rem;font-weight:bold',
+      'font-family:system-ui;font-size:1.5rem;font-weight:bold',
+      'color:#ffff00;font-family:system-ui;font-size:1.5rem;font-weight:bold',
+      'font-family:system-ui;font-size:1.5rem;font-weight:bold'
+    );
+  } else {
+    console.log(
+      '%cSUCCESS! %cSUCCESSFULLY CLEARED COMPLETED TASKS.',
+      'color:#00ff00;font-family:system-ui;font-size:1.5rem;font-weight:bold',
+      'font-family:system-ui;font-size:1.5rem;font-weight:bold'
+    );
+  }
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
